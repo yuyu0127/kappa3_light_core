ec8a293067431243c100092edabaa6ee319e33dc
unknown <1TE17236Y>
2019-06-13 16:53:03 +0900

    一旦クリア

--- kappa3_light_core.v
+++ kappa3_light_core.v
@@ -36,209 +36,179 @@
 // dbg_b_out:     デバッグ用のBレジスタ出力
 // dbg_c_out:     デバッグ用のCレジスタ出力
 // dbg_mem_out:   デバッグ用のメモリ出力
-module kappa3_light_core(input            clock,
-                         input            clock2,
-                         input            reset,
+module kappa3_light_core(input 	       clock,
+			 input 	       clock2,
+			 input 	       reset,
 
-                         // 実行制御      
-                         input            run,
-                         input            step_phase,
-                         input            step_inst,
-                   
-                         output [3:0]     cstate,
-                         output           running,
-                   
-                         // デバッグ関係
-                         input [31:0]     dbg_in,
-                         input            dbg_pc_ld,
-                         input            dbg_ir_ld,
-                         input            dbg_reg_ld,
-                         input [4:0]      dbg_reg_addr,
-                         input            dbg_a_ld,
-                         input            dbg_b_ld,
-                         input            dbg_c_ld,
-                         input [31:0]     dbg_mem_addr,
-                         input            dbg_mem_read,
-                         input            dbg_mem_write,
-                         output [31:0]    dbg_pc_out,
-                         output [31:0]    dbg_ir_out,
-                         output [31:0]    dbg_reg_out,
-                         output [31:0]    dbg_a_out,
-                         output [31:0]    dbg_b_out,
-                         output [31:0]    dbg_c_out,
-                         output [31:0]    dbg_mem_out);
+			 // 実行制御
+			 input 	       run,
+			 input 	       step_phase,
+			 input 	       step_inst,
+
+			 output [3:0]  cstate,
+			 output        running,
+
+			 // デバッグ関係
+			 input [31:0]  dbg_in,
+			 input 	       dbg_pc_ld,
+			 input 	       dbg_ir_ld,
+			 input 	       dbg_reg_ld,
+			 input [4:0]   dbg_reg_addr,
+			 input 	       dbg_a_ld,
+			 input 	       dbg_b_ld,
+			 input 	       dbg_c_ld,
+			 input [31:0]  dbg_mem_addr,
+			 input 	       dbg_mem_read,
+			 input 	       dbg_mem_write,
+			 output [31:0] dbg_pc_out,
+			 output [31:0] dbg_ir_out,
+			 output [31:0] dbg_reg_out,
+			 output [31:0] dbg_a_out,
+			 output [31:0] dbg_b_out,
+			 output [31:0] dbg_c_out,
+			 output [31:0] dbg_mem_out);
 
    // デバッグモードの信号
-   wire   dbg_mode;
+   wire 			       dbg_mode;
    assign dbg_mode = !running;
 
    // PC
-   wire [31:0]    pc_in;       // PC の書き込みデータ
-   wire           pc_ld;       // PC の書き込みイネーブル信号
-   wire [31:0]    pc;          // PC の値
-   assign pc_in = pc_sel ? creg : (pc + 4);
+   wire [31:0] 			pc_in;       // PC の書き込みデータ
+   wire 			pc_ld;       // PC の書き込みイネーブル信号
+   wire [31:0] 			pc;          // PC の値
    reg32 pc_inst(.clock(clock2),
-                 .reset(reset),
-                 .in(pc_in),
-                 .ld(pc_ld),
-                 .out(pc),
-                 .dbg_mode(dbg_mode),
-                 .dbg_in(dbg_in),
-                 .dbg_ld(dbg_pc_ld));
+		 .reset(reset),
+		 .in(pc_in),
+		 .ld(pc_ld),
+		 .out(pc),
+		 .dbg_mode(dbg_mode),
+		 .dbg_in(dbg_in),
+		 .dbg_ld(dbg_pc_ld));
    assign dbg_pc_out = pc;
 
    // IR
-   wire [31:0]    ir_in;      // IR の書き込みデータ
-   wire           ir_ld;      // IR の書き込みイネーブル信号
-   wire [31:0]    ir;         // IRの値
+   wire [31:0] 		 ir_in;      // IR の書き込みデータ
+   wire 		 ir_ld;      // IR の書き込みイネーブル信号
+   wire [31:0] 		 ir;         // IRの値
    reg32 ir_inst(.clock(clock2),
-                 .reset(reset),
-                 .in(ir_in),
-                 .ld(ir_ld),
-                 .out(ir),
-                 .dbg_mode(dbg_mode),
-                 .dbg_in(dbg_in),
-                 .dbg_ld(dbg_ir_ld));
+		 .reset(reset),
+		 .in(ir_in),
+		 .ld(ir_ld),
+		 .out(ir),
+		 .dbg_mode(dbg_mode),
+		 .dbg_in(dbg_in),
+		 .dbg_ld(dbg_ir_ld));
    assign dbg_ir_out = ir;
 
    // メモリ
-   wire [31:0]    mem_addr;
-   wire           mem_write;
-   wire [31:0]    mem_wrdata;
-   wire [3:0]     mem_wrbits;
-   wire [31:0]    mem_rddata;
-   assign mem_addr = mem_sel ? creg : pc;
+   wire [31:0] 		 mem_addr;
+   wire 		 mem_write;
+   wire [31:0] 		 mem_wrdata;
+   wire [3:0] 		 mem_wrbits;
+   wire [31:0] 		 mem_rddata;
    memory mem_inst(.clock(clock),
-                   .address(mem_addr),
-                   .read(mem_read),
-                   .write(mem_write),
-                   .wrdata(mem_wrdata),
-                   .wrbits(mem_wrbits),
-                   .rddata(mem_rddata),
-                   .dbg_address(dbg_mem_addr),
-                   .dbg_read(dbg_mem_read),
-                   .dbg_write(dbg_mem_write),
-                   .dbg_in(dbg_in),
-                   .dbg_out(dbg_mem_out));
-
-   // ST_CONV
-   stconv stconv_inst(.in(breg),
-                      .ir(ir),
-                      .out(mem_wrdata));
-                      
-   // LD_CONV
-   wire [31:0]    ldconv_out;
-   ldconv ldconv_inst(.in(rddata),
-                      .ir(ir),
-                      .offset(mem_sel ? creg : pc),
-                      .out(ldconv_out));
+		   .address(mem_addr),
+		   .read(1'b1),
+		   .write(mem_write),
+		   .wrdata(mem_wrdata),
+		   .wrbits(mem_wrbits),
+		   .rddata(mem_rddata),
+		   .dbg_address(dbg_mem_addr),
+		   .dbg_read(dbg_mem_read),
+		   .dbg_write(dbg_mem_write),
+		   .dbg_in(dbg_in),
+		   .dbg_out(dbg_mem_out));
 
    // reg-file
-   wire [4:0]     rs1_addr;     // rs1 のアドレス
-   wire [4:0]     rs2_addr;     // rs2 のアドレス
-   wire [4:0]     rd_addr;      // rd のアドレス
-   wire [31:0]    rd_in;        // rd に書き込む値
-   wire           rd_ld;        // rd の書込みイネーブル信号
-   wire [31:0]    rs1;          // rs1 の値
-   wire [31:0]    rs2;          // rs2 の値
-   assign rd_in = 
-      (rd_sel == 0) ? ldconv_out : (
-      (rd_sel == 1) ? pc : (
-      (rd_sel == 2) ? creg : (
-      0
-   )));
+   wire [4:0] 		 rs1_addr;     // rs1 のアドレス
+   wire [4:0] 		 rs2_addr;     // rs2 のアドレス
+   wire [4:0] 		 rd_addr;      // rd のアドレス
+   wire [31:0] 		 rd_in;        // rd に書き込む値
+   wire                  rd_ld;        // rd の書込みイネーブル信号
+   wire [31:0] 		 rs1;          // rs1 の値
+   wire [31:0] 		 rs2;          // rs2 の値
    regfile regfile_inst(.clock(clock2),
-                        .reset(reset),
-                        .rs1_addr(rs1_addr),
-                        .rs2_addr(rs2_addr),
-                        .rd_addr(rd_addr),
-                        .in(rd_in),
-                        .ld(rd_ld),
-                        .rs1_out(rs1),
-                        .rs2_out(rs2),
-                        .dbg_mode(dbg_mode),
-                        .dbg_in(dbg_in),
-                        .dbg_addr(dbg_reg_addr),
-                        .dbg_ld(dbg_reg_ld),
-                        .dbg_out(dbg_reg_out));
+			.reset(reset),
+			.rs1_addr(rs1_addr),
+			.rs2_addr(rs2_addr),
+			.rd_addr(rd_addr),
+			.in(rd_in),
+			.ld(rd_ld),
+			.rs1_out(rs1),
+			.rs2_out(rs2),
+			.dbg_mode(dbg_mode),
+			.dbg_in(dbg_in),
+			.dbg_addr(dbg_reg_addr),
+			.dbg_ld(dbg_reg_ld),
+			.dbg_out(dbg_reg_out));
 
    // A-reg
-   wire           a_ld;         // A-reg の書込みイネーブル信号
-   wire [31:0]    areg;         // A-reg の値
+   wire                  a_ld;         // A-reg の書込みイネーブル信号
+   wire [31:0] 		 areg;         // A-reg の値
    reg32 areg_inst(.clock(clock2),
-                   .reset(reset),
-                   .in(rs1),
-                   .ld(a_ld),
-                   .out(areg),
-                   .dbg_mode(dbg_mode),
-                   .dbg_in(dbg_in),
-                   .dbg_ld(dbg_a_ld));
+		   .reset(reset),
+		   .in(rs1),
+		   .ld(a_ld),
+		   .out(areg),
+		   .dbg_mode(dbg_mode),
+		   .dbg_in(dbg_in),
+		   .dbg_ld(dbg_a_ld));
    assign dbg_a_out = areg;
 
    // B-reg
-   wire           b_ld;         // B-reg の書込みイネーブル信号
-   wire [31:0]    breg;         // B-reg の値
+   wire                  b_ld;         // B-reg の書込みイネーブル信号
+   wire [31:0] 		 breg;         // B-reg の値
    reg32 breg_inst(.clock(clock2),
-                   .reset(reset),
-                   .in(rs2),
-                   .ld(b_ld),
-                   .out(breg),
-                   .dbg_mode(dbg_mode),
-                   .dbg_in(dbg_in),
-                   .dbg_ld(dbg_b_ld));
+		   .reset(reset),
+		   .in(rs2),
+		   .ld(b_ld),
+		   .out(breg),
+		   .dbg_mode(dbg_mode),
+		   .dbg_in(dbg_in),
+		   .dbg_ld(dbg_b_ld));
    assign dbg_b_out = breg;
 
-   wire [ 3:0]    alu_ctl;
-   wire [31:0]    alu_out;    // ALU の出力
-
-   alu alu_inst(.in1(a_sel ? pc_in : areg),
-                .in2(b_sel ? imm : breg),
-                .ctl(alu_ctl),
-                .out(alu_out));
+   // ALU
+   wire [31:0] 		 alu_out;      // ALU の出力
+   // 実際にはここに alu のインスタンス記述が入る．
 
    // C-reg
-   wire           c_ld;         // C-reg の書込みイネーブル信号
-   wire [31:0]    creg;         // C-reg の値
+   wire                  c_ld;         // C-reg の書込みイネーブル信号
+   wire [31:0] 		 creg;         // C-reg の値
+   reg32 creg_inst(.clock(clock2),
+		   .reset(reset),
+		   .in(alu_out),
+		   .ld(c_ld),
+		   .out(creg),
+		   .dbg_mode(dbg_mode),
+		   .dbg_in(dbg_in),
+		   .dbg_ld(dbg_c_ld));
    assign dbg_c_out = creg;
 
-   // Controller
-   wire           pc_sel;
-   wire           mem_sel;
-   wire           mem_read;
-   wire [1:0]     rd_sel;
-   wire           a_sel;
-   wire           b_sel;
-   wire [31:0]    imm;
+   // 制御信号はすべて0にしておく．
+   assign pc_in = 32'b0;
+   assign pc_ld = 1'b0;
+   assign ir_in = 32'b0;
+   assign ir_ld = 1'b0;
+   assign mem_addr = 32'b0;
+   assign mem_write = 1'b0;
+   assign mem_wrdata = 32'b0;
+   assign mem_wrbits = 4'b0;
+   assign rs1_addr = 5'b0;
+   assign rs2_addr = 5'b0;
+   assign rd_addr = 5'b0;
+   assign rd_in = 32'b0;
+   assign rd_ld = 1'b0;
+   assign a_ld = 1'b0;
+   assign b_ld = 1'b0;
+   assign alu_out = 32'b0;
+   assign c_ld = 1'b0;
+   assign cstate = 4'b0000; // この値もダミー
 
-   controller controller_inst(.cstate(cstate),
-                              .ir(ir),
-                              .addr(address),
-                              .alu_out(alu_out),
-                              .pc_sel(pc_sel),
-                              .pc_ld(pc_ld),
-                              .mem_sel(mem_sel),
-                              .mem_read(mem_read),
-                              .mem_write(mem_write),
-                              .mem_wrbits(mem_wrbits),
-                              .ir_ld(ir_ld),
-                              .rs1_addr(rs1_addr),
-                              .rs2_addr(rs2_addr),
-                              .rd_addr(rd_addr),
-                              .rd_sel(rd_sel),
-                              .rd_ld(rd_ld),
-                              .a_ld(a_ld),
-                              .b_ld(b_ld),
-                              .a_sel(a_sel),
-                              .b_sel(b_sel),
-                              .imm(imm),
-                              .alu_ctl(alu_ctl),
-                              .c_ld(c_ld));
-
-   phasegen phasegen_inst(.clock(clock2), 
-                          .reset(reset),
-                          .run(run), .step_phase(step_phase),
-                          .step_inst(step_inst),
-                          .cstate(cstate), 
-                          .running(running));
+   // running は実際には phasegen の出力を用いる．
+   // phasegen phasegen_inst(.clock(clock2), .reset(reset),
+   //			  .run(run), .step_phase(step_phase), .step_inst(step_inst),
+   //			  .cstate(cstate), .running(running));
+   assign running = 1'b0;
 
 endmodule // kappa3_light_core
